97553dba403f6404c49e120e39fa33952dcdfc58
Akash Sahu <akashsahu793@gmail.com>
2024-05-06 21:53:31 +0530

    added multiple middlewares

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 
+// middlewares
+app.use(
+  cors({
+    origin: ["http://localhost:8000", "http://localhosthost:5173"],
+    credentials: true,
+  })
+);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); //watch it
+  res.header([
+    "Access-Control-Allow-Headers",
+    "Origin, x-Requested-With, Content-Type, Accept",
+  ]);
+  next();
+});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/upload", express.static("uploads"));
+
+// path
 app.get("/", (req, res) => {
   res.json({ message: "welcome to webdev hell" });
 });
